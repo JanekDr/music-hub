3a1faa81ff21685ca5ef00c1ba9347aef84fbe84
Jan Druszcz <jan.druszcz@onet.eu>
2025-10-17 16:34:33 +0200

    feat: playlist, track and queue model to admin dashboard

--- backend/music_hub/playlist/admin.py
+++ backend/music_hub/playlist/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Playlist, Queue, Track
+
 # Register your models here.
+admin.site.register(Playlist)
+admin.site.register(Queue)
+admin.site.register(Track)
