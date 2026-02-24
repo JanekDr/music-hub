b528b7bafd8714dce11a79283e5acda237c57f9c
Jan Druszcz <jan.druszcz@onet.eu>
2026-02-24 16:05:36 +0100

    feat: add landing page

--- backend/music_hub/playlist/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class PlaylistConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "playlist"
-
-    def ready(self):
-        from . import signals
--- backend/music_hub/soundcloud/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import SoundcloudToken
-
-# Register your models here.
-admin.site.register(SoundcloudToken)
